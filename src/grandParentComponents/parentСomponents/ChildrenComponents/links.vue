0a92afdd053441f9224e217308b9c0224e71bacc
magoter <149555824+magoter@users.noreply.github.com>
2023-11-01 07:21:55 +0300

    Add files via upload

---
+++ src/grandParentComponents/parentСomponents/ChildrenComponents/links.vue
@@ -0,0 +1,32 @@
+<template >
+    <div class="Links">
+        <router-link :to="{name: 'Home'}" class="isActive">Домашняя страница</router-link>
+        <router-link :to="{name: 'Chat'}" class="isActive">Чат</router-link>
+        <router-link :to="{name: 'Maps'}" class="isActive">Карты</router-link>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: "LinksConmponent"
+    }
+</script>
+
+<style scoped>
+    .Links{
+        width:800px;
+        display:flex;
+        justify-content: space-between;
+    }
+    .isActive{
+        text-decoration: none;
+        color: azure;
+        width: 200px;
+        height: 40px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: rgba(31, 31, 31, 0.5);
+        border-radius: 20px;
+    }
+</style>
